3b2270af58c84158493646cea076f1147def6f85
jaycedowell <jdowell@unm.edu>
2014-10-11 16:17:47 -0600

    Added a function, getHistroy(), to access the 'history' feature in the WUnderground API and updated the getWeatherAdjustment() function to use this.  This should provide a better idea of what the mean temperature/humidity is for the current date.  Also, added in internal query rate limiter to the code to make sure that we don't push more than 5 per minute (the limit of the free API key).

--- weather.py
+++ weather.py
@@ -5,15 +5,80 @@ Module for reading in weather conditions from Weather Underground using their AP
 """
 
 import json
+import time
 import urllib
+from datetime import datetime, timedelta
 
-__version__ = "0.1"
-__all__ = ["getCurrentConditions", "getYesterdaysConditions", "getCurrentTemperature", 
+__version__ = "0.2"
+__all__ = ["getCurrentConditions", "getYesterdaysConditions", "getHistory", "getCurrentTemperature", 
 		   "getWeatherAdjustment", "__version__", "__all__"]
 
 
 # Base URL for all queries
 _baseURL = "http://api.wunderground.com/api"
+
+
+# Rate limiter
+class _rateLimiter(object):
+	"""
+	Class to help make sure that the various calls to the WUnderground API don't exceed 
+	the free rate limit of 5 queries per minute.  This class can also be adjusted to 
+	work with other rate limits as well.
+	"""
+	
+	def __init__(self, requestsPerMinute=5):
+		self.requestsPerMinute = float(requestsPerMinute)
+		
+		self._requests = []
+		
+	def clearToRequest(self, block=True):
+		"""
+		Find out if sending a request now would be allowable.  Return True
+		if it is, False otherwise.  If 'block' is set to True, the function
+		blocks until it is clear to make a request again.
+		"""
+		
+		# Get the current time
+		tNow = time.time()
+		
+		# Find out how many requests have been make in the last minute and make of list
+		# of requests that should be purged
+		requestCount = 0
+		requestsToRemove = []
+		for t in self._requests:
+			if tNow - t <= 60.0:
+				requestCount += 1
+			else:
+				requestsToRemove.append(t)
+				
+		# Clean out the old requests
+		for t in requestsToRemove:
+			idx = self._requests.index(t)
+			del self._requests[idx]
+			
+		# Can we make another request?
+		clear = False
+		if requestCount < self.requestsPerMinute:
+			## Yes, update the requests list
+			self._requests.append( tNow )
+			clear = True
+		else:
+			if block:
+				## Sleep for a bit and try again
+				time.sleep(5)
+				while not self.clearToRequest(block=False):
+					time.sleep(5)
+					
+				clear = True
+			else:
+				## Nope
+				clear = False
+				
+		return clear
+
+
+# Create the rate limiter
+_rl = _rateLimiter()
 
 
 def getCurrentConditions(apiKey, pws=None, postal=None):
@@ -32,6 +97,9 @@ def getCurrentConditions(apiKey, pws=None, postal=None):
 	else:
 		raise RuntimeError("Must specify either a PWS ID or a postal code")
 		
+	# Check the rate limiter
+	_rl.clearToSend()
+	
 	uh = urllib.urlopen(url)
 	data = json.loads(uh.read())
 	uh.close()
@@ -55,6 +123,46 @@ def getYesterdaysConditions(apiKey, pws=None, postal=None):
 	else:
 		raise RuntimeError("Must specify either a PWS ID or a postal code")
 		
+	# Check the rate limiter
+	_rl.clearToSend()
+	
+	uh = urllib.urlopen(url)
+	data = json.loads(uh.read())
+	uh.close()
+	
+	return data
+
+
+def getHistory(apiKey, date, pws=None, postal=None):
+	"""
+	Get the weather history for the specified date/YYYYMMDD date string 
+	of the personal weather station or postal code using the 
+	WUndergroundAPI.
+	
+	.. note::
+		This requires a valid API key with history to work.
+	"""
+	
+	# Process the date
+	if type(date) is datetime:
+		date = date.strftime("%Y%m%d")
+	else:
+		try:
+			date = datetime.strptime(date, "%Y%m%d")
+			date = date.strftime("%Y%m%d")
+		except ValueError:
+			raise ValueError("date does not appears to be in YYYYMMDD format")
+			
+	if pws is not None and pws != '':
+		url = "%s/%s/history_%s/q/pws:%s.json" % (_baseURL, apiKey, date, pws)
+	elif postal is not None and postal != '':
+		url = "%s/%s/history_%s/q/%s.json" % (_baseURL, apiKey, date, str(postal))
+	else:
+		raise RuntimeError("Must specify either a PWS ID or a postal code")
+		
+	# Check the rate limiter
+	_rl.clearToSend()
+	
 	uh = urllib.urlopen(url)
 	data = json.loads(uh.read())
 	uh.close()
@@ -80,26 +188,21 @@ def getWeatherAdjustment(apiKey, pws=None, postal=None):
 	Compute a watering time scale factor using the WUnderground conditions.
 	"""
 	
-	cNow = getCurrentConditions(apiKey, pws=pws, postal=postal)
-	cPast = getYesterdaysConditions(apiKey, pws=pws, postal=postal)
+	# Today
+	tNow = datetime.now()
+	cNow = getHistroy(apiKey, tNow, pws=pws, postal=postal)
+	tNow = float(cNow['dailysummary']['meantempi'])
+	hNow = float(cNow['dailysummary']['humidity'])
+	pNow = float(cNow['dailysummary']['precipi'])
 	
-	tNow = float(cNow['current_observation']['temp_f'])
-	hNow = float(cNow['current_observation']['relative_humidity'].replace('%', ''))
-	pNow = float(cNow['current_observation']['precip_today_in'])
+	# Yesterday
+	tPast  = tNow - timedelta(days=1)
+	cPast = getHistory(apiKey, tYesterday, pws=pws, postal=postal)
+	tPast = float(cPast['dailysummary']['meantempi'])
+	hPast = float(cPast['dailysummary']['humidity'])
+	pPast = float(cPast['dailysummary']['precipi'])
 	
-	tPast = 0.0
-	hPast = 0.0
-	pPast = 0.0
-	pCount = 0
-	for obs in cPast['history']['observations']:
-		tPast += float(obs['tempi'])
-		hPast += float(obs['hum'])
-		pPast += float(obs['precipi']) if float(obs['precipi']) > 0.0 else 0.0
-		pCount += 1
-	if pCount > 0:
-		tPast /= pCount
-		hPast /= pCount
-		
+	# The various bits of the scaling relation
 	tFactor = 4.0*((0.5*tNow + 0.5*tPast) - 70.0)	# percent
 	rFactor = 1.0*(30.0 - (0.5*hNow + 0.5*hPast))	# percent
 	pFactor = -2.0*((pNow + pPast)*100.0)			# percent
