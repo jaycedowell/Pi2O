94976edb3ddc9013d2a5ac0836bf46b50fe0d31b
jaycedowell <jayce.dowell@gmail.com>
2022-06-26 07:26:17 -0600

    Import fix.

--- database.py
+++ database.py
@@ -105,7 +105,7 @@ class DatabaseProcessor(object):
                 exc_type, exc_value, exc_traceback = sys.exc_info()
                 _LOGGER.error("DatabaseProcessor: %s at line %i", e, exc_traceback.tb_lineno)
                 ## Grab the full traceback and save it to a string via StringIO
-                fileObject = StringIO.StringIO()
+                fileObject = StringIO()
                 traceback.print_tb(exc_traceback, file=fileObject)
                 tbString = fileObject.getvalue()
                 fileObject.close()
