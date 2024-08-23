659c4ee3ce5ed57e9c966b6df31e26bb791a051b
jaycedowell <jayce.dowell@gmail.com>
2024-08-23 06:15:01 -0600

    Documentation, plus a method naming fix.

--- zone.py
+++ zone.py
@@ -76,6 +76,14 @@ class SprinklerZone(object):
     """
     
     def __init__(self, relay, rate=0.0, current_et_value=0.0):
+        """
+        Create a new SprinklerZone using:
+         * relay - a relay GPIO pin number or GPIORelay instance,
+         * rate - zone precipation rate in [inches or mm] per hour, and
+         * current_et_value - current evapotranspiration value in the same
+                              same volume units as "rate".
+        """
+        
         if type(relay) == GPIORelay:
             self.relay = relay
         else:
@@ -89,6 +97,10 @@ class SprinklerZone(object):
         self.lastStop = 0
         
     def on(self):
+        """
+        Turn the sprinkler zone on.
+        """
+        
         if self.state == 0:
             self.relay.on()
             _LOGGER.info('Turned on GPIO pin %i', self.relay.pin)
@@ -97,6 +109,10 @@ class SprinklerZone(object):
             self.lastStart = time.time()
             
     def off(self):
+        """
+        Turn the sprinkler zone off.
+        """
+        
         if self.state == 1:
             self.relay.off()
             _LOGGER.info('Turned off GPIO pin %i', self.relay.pin)
@@ -106,14 +122,38 @@ class SprinklerZone(object):
             
     @property
     def is_active(self):
+        """
+        Whether or not the zone is currently on.
+        """
+        
         return True if self.state else False
         
     def get_last_run(self):
+        """
+        Return the timestamp of when the zone was last turned on.
+        """
+        
         return self.lastStart
         
-    def get_durations_from_precipitation(self, precip):
+    def get_duration_from_precipitation(self, precip):
+        """
+        Given a desired precipation amount in inches/mm, return how many minutes
+        the zone will need to be on in order to deliever that.
+        
+        .. note:: The units for the precipation are tied to whatever was used
+                  when initializing the 'rate' keyword.
+        """
+        
         return precip / self.rate * 60.0
         
-    def get_precipitation_from_durations(self, duration_min):
+    def get_precipitation_from_duration(self, duration_min):
+        """
+        Given a zone run time in minutes, return how much water was delievered
+        in inches/mm.
+        
+        .. note:: The units for the precipation are tied to whatever was used
+                  when initializing the 'rate' keyword.
+        """
+        
         return self.rate * duration_min / 60.0
         
